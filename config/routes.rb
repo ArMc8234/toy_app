ee11002a8f5dde71882726170652be57c63c3617
Aaron McKoy <53494682+ArMc8234@users.noreply.github.com>
2022-06-08 11:56:06 -0400

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Redirects to render "hello, world" as home page
+  root 'application#hello'
 end
